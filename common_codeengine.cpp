b044de8fea8e8d81fb16dc0b7a50cb0afedc581f
jimingmin <jimingmin@gmail.com>
2015-02-03 19:07:54 +0800

    编解码增加c接口

--- common_codeengine.cpp
+++ common_codeengine.cpp
@@ -341,3 +341,133 @@ int32_t CCodeEngine::Decode(const uint8_t *buf, const uint32_t size, uint32_t& o
 
 	return S_OK;
 }
+
+int32_t encode_bool(uint8_t* buf, const uint32_t size, uint32_t* offset, const bool data)
+{
+	return CCodeEngine::Encode(buf, size, *offset, data);
+}
+
+int32_t encode_int8(uint8_t* buf, const uint32_t size, uint32_t* offset, const int8_t data)
+{
+	return CCodeEngine::Encode(buf, size, *offset, data);
+}
+
+int32_t encode_int16(uint8_t* buf, const uint32_t size, uint32_t* offset, const int16_t data)
+{
+	return CCodeEngine::Encode(buf, size, *offset, data);
+}
+
+int32_t encode_int32(uint8_t* buf, const uint32_t size, uint32_t* offset, const int32_t data)
+{
+	return CCodeEngine::Encode(buf, size, *offset, data);
+}
+
+int32_t encode_int64(uint8_t* buf, const uint32_t size, uint32_t* offset, const int64_t data)
+{
+	return CCodeEngine::Encode(buf, size, *offset, data);
+}
+
+int32_t encode_uint8(uint8_t* buf, const uint32_t size, uint32_t* offset, const uint8_t data)
+{
+	return CCodeEngine::Encode(buf, size, *offset, data);
+}
+
+int32_t encode_uint16(uint8_t* buf, const uint32_t size, uint32_t* offset, const uint16_t data)
+{
+	return CCodeEngine::Encode(buf, size, *offset, data);
+}
+
+int32_t encode_uint32(uint8_t* buf, const uint32_t size, uint32_t* offset, const uint32_t data)
+{
+	return CCodeEngine::Encode(buf, size, *offset, data);
+}
+
+int32_t encode_uint64(uint8_t* buf, const uint32_t size, uint32_t* offset, const uint64_t data)
+{
+	return CCodeEngine::Encode(buf, size, *offset, data);
+}
+
+int32_t encode_uint8_mem(uint8_t* buf, const uint32_t size, uint32_t* offset, const uint8_t *data, const uint32_t length, const uint32_t maxLength)
+{
+	return CCodeEngine::Encode(buf, size, *offset, data, length, maxLength);
+}
+
+int32_t encode_char_mem(uint8_t* buf, const uint32_t size, uint32_t* offset, const char *data, const uint16_t maxLength)
+{
+	return CCodeEngine::Encode(buf, size, *offset, data, maxLength);
+}
+
+int32_t encode_double(uint8_t* buf, const uint32_t size, uint32_t* offset, const double data)
+{
+	return CCodeEngine::Encode(buf, size, *offset, data);
+}
+
+//int32_t Encode(uint8_t* buf, const uint32_t size, uint32_t* offset, const string &data)
+//{
+//	CCodeEngine::Encode(buf, size, *offset, data);
+//}
+
+int32_t decode_bool(const uint8_t *buf, const uint32_t size, uint32_t* offset, bool* data)
+{
+	return CCodeEngine::Decode(buf, size, *offset, *data);
+}
+
+int32_t decode_int8(const uint8_t *buf, const uint32_t size, uint32_t* offset, int8_t* data)
+{
+	return CCodeEngine::Decode(buf, size, *offset, *data);
+}
+
+int32_t decode_int16(const uint8_t *buf, const uint32_t size, uint32_t* offset, int16_t* data)
+{
+	return CCodeEngine::Decode(buf, size, *offset, *data);
+}
+
+int32_t decode_int32(const uint8_t *buf, const uint32_t size, uint32_t* offset, int32_t* data)
+{
+	return CCodeEngine::Decode(buf, size, *offset, *data);
+}
+
+int32_t decode_int64(const uint8_t *buf, const uint32_t size, uint32_t* offset, int64_t* data)
+{
+	return CCodeEngine::Decode(buf, size, *offset, *data);
+}
+
+int32_t decode_uint8(const uint8_t *buf, const uint32_t size, uint32_t* offset, uint8_t* data)
+{
+	return CCodeEngine::Decode(buf, size, *offset, *data);
+}
+
+int32_t decode_uint16(const uint8_t *buf, const uint32_t size, uint32_t* offset, uint16_t* data)
+{
+	return CCodeEngine::Decode(buf, size, *offset, *data);
+}
+
+int32_t decode_uint32(const uint8_t *buf, const uint32_t size, uint32_t* offset, uint32_t* data)
+{
+	return CCodeEngine::Decode(buf, size, *offset, *data);
+}
+
+int32_t decode_uint64(const uint8_t *buf, const uint32_t size, uint32_t* offset, uint64_t* data)
+{
+	return CCodeEngine::Decode(buf, size, *offset, *data);
+}
+
+int32_t decode_uint8_mem(const uint8_t *buf, const uint32_t size, uint32_t* offset, uint8_t* data, const uint32_t length, const uint32_t maxLength)
+{
+	return CCodeEngine::Decode(buf, size, *offset, data, length, maxLength);
+}
+
+int32_t decode_char_mem(const uint8_t *buf, const uint32_t size, uint32_t* offset, char* data, const uint16_t maxLength)
+{
+	return CCodeEngine::Decode(buf, size, *offset, data, maxLength);
+}
+
+int32_t decode_double(const uint8_t *buf, const uint32_t size, uint32_t* offset, double* data)
+{
+	return CCodeEngine::Decode(buf, size, *offset, *data);
+}
+
+//int32_t Decode(const uint8_t *buf, const uint32_t size, uint32_t& offset, string &data)
+//{
+//	return CCodeEngine::Decode(buf, size, *offset, *data);
+//}
--- common_codeengine.h
+++ common_codeengine.h
@@ -82,5 +82,34 @@ protected:
 	static CEndian	ms_endian;
 };
 
+extern "C"
+{
+EXPORT int32_t encode_bool(uint8_t* buf, const uint32_t size, uint32_t* offset, const bool data);
+EXPORT int32_t encode_int8(uint8_t* buf, const uint32_t size, uint32_t* offset, const int8_t data);
+EXPORT int32_t encode_int16(uint8_t* buf, const uint32_t size, uint32_t* offset, const int16_t data);
+EXPORT int32_t encode_int32(uint8_t* buf, const uint32_t size, uint32_t* offset, const int32_t data);
+EXPORT int32_t encode_int64(uint8_t* buf, const uint32_t size, uint32_t* offset, const int64_t data);
+EXPORT int32_t encode_uint8(uint8_t* buf, const uint32_t size, uint32_t* offset, const uint8_t data);
+EXPORT int32_t encode_uint16(uint8_t* buf, const uint32_t size, uint32_t* offset, const uint16_t data);
+EXPORT int32_t encode_uint32(uint8_t* buf, const uint32_t size, uint32_t* offset, const uint32_t data);
+EXPORT int32_t encode_uint64(uint8_t* buf, const uint32_t size, uint32_t* offset, const uint64_t data);
+EXPORT int32_t encode_uint8_mem(uint8_t* buf, const uint32_t size, uint32_t* offset, const uint8_t *data, const uint32_t length, const uint32_t maxLength);
+EXPORT int32_t encode_char_mem(uint8_t* buf, const uint32_t size, uint32_t* offset, const char *data, const uint16_t maxLength);
+EXPORT int32_t encode_double(uint8_t* buf, const uint32_t size, uint32_t* offset, const double data);
+
+EXPORT int32_t decode_bool(const uint8_t *buf, const uint32_t size, uint32_t* offset, bool* data);
+EXPORT int32_t decode_int8(const uint8_t *buf, const uint32_t size, uint32_t* offset, int8_t* data);
+EXPORT int32_t decode_int16(const uint8_t *buf, const uint32_t size, uint32_t* offset, int16_t* data);
+EXPORT int32_t decode_int32(const uint8_t *buf, const uint32_t size, uint32_t* offset, int32_t* data);
+EXPORT int32_t decode_int64(const uint8_t *buf, const uint32_t size, uint32_t* offset, int64_t* data);
+EXPORT int32_t decode_uint8(const uint8_t *buf, const uint32_t size, uint32_t* offset, uint8_t* data);
+EXPORT int32_t decode_uint16(const uint8_t *buf, const uint32_t size, uint32_t* offset, uint16_t* data);
+EXPORT int32_t decode_uint32(const uint8_t *buf, const uint32_t size, uint32_t* offset, uint32_t* data);
+EXPORT int32_t decode_uint64(const uint8_t *buf, const uint32_t size, uint32_t* offset, uint64_t* data);
+EXPORT int32_t decode_uint8_mem(const uint8_t *buf, const uint32_t size, uint32_t* offset, uint8_t* data, const uint32_t length, const uint32_t maxLength);
+EXPORT int32_t decode_char_mem(const uint8_t *buf, const uint32_t size, uint32_t* offset, char* data, const uint16_t maxLength);
+EXPORT int32_t decode_double(const uint8_t *buf, const uint32_t size, uint32_t* offset, double* data);
+
+}
 
 #endif /* COMMON_CODEENGINE_H_ */
